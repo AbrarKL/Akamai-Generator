137f4247b0b3a426dec270b378ac2b4105cdb47c
AbrarKL <abrarlone01@outlook.com>
2018-05-25 00:17:37 +0100

    Added mouse simulation

--- mouseMovementSimulation.js
+++ mouseMovementSimulation.js
@@ -8,27 +8,20 @@ var ta = 0;
 var mduce_cnt = 0;
 var mduce_cnt_lmt = 75;
 
-function hmm(i) {
-    cma(i, 1);
-}
+//simulate Mouse movement
+simulateCMA(200, 200, 1) // state 1 = its movement
+simulateCMA(200, 201, 1) // Moved x axis forward
+simulateCMA(200, 202, 1) // Moved x axis forward again
+simulateCMA(200, 203, 1) // Moved x axis forward again (so it seems legitimate)
+console.log("Mouse Movement Count: " + mme_cnt);
+simulateCMA(200, 202, 3) // Mouse pressed / mousedown
+simulateCMA(200, 202, 4) // Mouse released / mouseup
+simulateCMA(200, 202, 2) // Mouse click / click
+console.log("Mouse Click Count: " + mduce_cnt);
 
-function hc(httpCode) {
-    cma(httpCode, 2);
-}
+console.log(mact);
 
-function hmd(mmCoreSplitViewBlock) {
-    cma(mmCoreSplitViewBlock, 3);
-}
-
-function hmu(mmCoreSplitViewBlock) {
-    cma(mmCoreSplitViewBlock, 4);
-}
-document["addEventListener"]("mousemove", hmm, true);
-document["addEventListener"]("click", hc, true);
-document["addEventListener"]("mousedown", hmd, true);
-document["addEventListener"]("mouseup", hmu, true);
-
-function cma(_props, _state) {
+function simulateCMA(xCoord, yCoord, _state) {
 	// Events in order. _state 1 should happen more than once
 	// _state 1 = mouse movement
 	// _state 3 = mouse pressed on element
@@ -38,26 +31,15 @@ function cma(_props, _state) {
 	// If mouse moved && mouse movement count is less than the mouse movement count limit (25 movements)
 	// OR if state isn't 1 and mduce_cnt is less than hte mduce_cnt limit which is 75
     if (1 == _state && mme_cnt < mme_cnt_lmt || 1 != _state && mduce_cnt < mduce_cnt_lmt) {
-        var props = _props || window["event"];
         var hide = -1;
         var i = -1;
 		// if props variable exists (not needed in our simulated one) and propsx exist and propsy exist (they should always)
-        if (props && props["pageX"] && props["pageY"]) {
-			// props["pageX"] will be replaced with a random x coordinate
-			// props["pageY"] will be replaecd with a random y coordinate
-            hide = Math["floor"](props["pageX"]);
-            i = Math["floor"](props["pageY"]);
-        } else {
-            if (props && props["clientX"] && props["clientY"]) {
-                hide = Math["floor"](props["clientX"]);
-                i = Math["floor"](props["clientY"]);
-            }
-        }
-        var value = props["toElement"];
-        if (null == value) {
-            value = props["target"];
-        }
-        var str = gf(value);
+		// props["pageX"] will be replaced with a random x coordinate
+		// props["pageY"] will be replaecd with a random y coordinate
+        hide = Math["floor"](xCoord);
+        i = Math["floor"](yCoord);
+        var value = undefined;
+        var str = value; // Was var str = gf(value);
 		// str returns undefined for some reason, so I assume we dont need above lines from var value = x etc
         var index = get_cf_date() - start_ts;
         var d = me_cnt + "," + _state + "," + index + "," + hide + "," + i;
@@ -65,7 +47,15 @@ function cma(_props, _state) {
             d = d + "," + str;	
 			//h = 0 if mouse moved
 			//h = 1 if mouse clicked
-            var h = void 0 !== props["which"] ? props["which"] : props["button"];
+			var h = 0;
+			if(_state == 1)
+			{
+				h = 0;
+			}
+			else
+			{
+				h = 1;
+			}
             if (null != h && 1 != h) {
                 d = d + "," + h;
             }
@@ -83,12 +73,12 @@ function cma(_props, _state) {
     me_cnt++;
 }
 
-function gf(a) {
+/*function gf(a) {
     var rv;
     if (rv = null == a ? document["activeElement"] : a, null == document["activeElement"]) {
         return -1;
     }
-}
+}*/
 
 function get_cf_date() {
     return Date["now"] ? Date["now"]() : +new Date;
